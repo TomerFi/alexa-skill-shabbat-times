8f5f99a6576dd7c5c93c9d0268676d9b3f6e7467
Tomer Figenblat <tomer.figenblat@gmail.com>
2019-10-10 12:04:50 +0300

    Added city locator static tool.

--- src/main/java/info/tomfi/alexa/skills/shabbattimes/request/handlers/GetCityIntentHandler.java
+++ src/main/java/info/tomfi/alexa/skills/shabbattimes/request/handlers/GetCityIntentHandler.java
@@ -1,12 +1,11 @@
 package info.tomfi.alexa.skills.shabbattimes.request.handlers;
 
+import static info.tomfi.alexa.skills.shabbattimes.tools.CityLocator.getByCityAndCountry;
 import static info.tomfi.alexa.skills.shabbattimes.tools.GlobalEnums.Attributes;
-import static info.tomfi.alexa.skills.shabbattimes.tools.GlobalEnums.CountryInfo;
 import static info.tomfi.alexa.skills.shabbattimes.tools.GlobalEnums.Intents;
 import static info.tomfi.alexa.skills.shabbattimes.tools.GlobalEnums.Slots;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,8 +18,6 @@ import com.amazon.ask.model.Slot;
 
 import info.tomfi.alexa.skills.shabbattimes.annotation.IncludeRequestHandler;
 import info.tomfi.alexa.skills.shabbattimes.city.City;
-import info.tomfi.alexa.skills.shabbattimes.country.Country;
-import info.tomfi.alexa.skills.shabbattimes.country.CountryFactory;
 import info.tomfi.alexa.skills.shabbattimes.exception.NoCityFoundException;
 import info.tomfi.alexa.skills.shabbattimes.exception.NoCityInCountryException;
 import info.tomfi.alexa.skills.shabbattimes.exception.NoCitySlotException;
@@ -35,7 +32,7 @@ public final class GetCityIntentHandler implements IntentRequestHandler
     }
 
     @Override
-    public Optional<Response> handle(final HandlerInput input, final IntentRequest intent) throws NoCitySlotException, NoCityInCountryException
+    public Optional<Response> handle(final HandlerInput input, final IntentRequest intent) throws NoCitySlotException, NoCityInCountryException, NoCityFoundException
     {
         final List<String> cityKeys = Arrays.asList(Slots.CITY_IL.name, Slots.CITY_GB.name, Slots.CITY_US.name);
         final Map<String, Slot> slots = intent.getIntent().getSlots();
@@ -63,53 +60,5 @@ public final class GetCityIntentHandler implements IntentRequestHandler
             .withSpeech("TODO found city")
             .withShouldEndSession(true)
             .build();
-    }
-
-    private City getByCityAndCountry(final Slot countrySlot, final Slot citySlot) throws NoCityFoundException, NoCityInCountryException
-    {
-        if (countrySlot.getValue() == null)
-        {
-            return getByCity(citySlot);
-        }
-        final Country country = CountryFactory.getCountry(countrySlot.getValue());
-        final Optional<City> cityOpt = findCityInCountry(country, citySlot.getValue());
-        if (cityOpt.isPresent())
-        {
-            return cityOpt.get();
-        }
-        throw new NoCityInCountryException(String.format("no city %s in %s.", citySlot.getValue(), countrySlot.getValue()));
-    }
-
-    private City getByCity(final Slot citySlot)
-    {
-        for (CountryInfo member : CountryInfo.values())
-        {
-            final Country country = CountryFactory.getCountryByMember(member);
-            final Optional<City> cityOpt = findCityInCountry(country, citySlot.getValue());
-            if (cityOpt.isPresent())
-            {
-                return cityOpt.get();
-            }
-        }
-        throw new NoCityFoundException(String.format("city %s not found", citySlot.getValue()));
-    }
-
-    private Optional<City> findCityInCountry(final Country country, final String cityName)
-    {
-        final Iterator<City> cities = country.iterator();
-        while (cities.hasNext())
-        {
-            final City currentCity = cities.next();
-            final Iterator<String> aliases = currentCity.iterator();
-            while (aliases.hasNext())
-            {
-                final String currentAlias = aliases.next();
-                if (currentAlias.equalsIgnoreCase(cityName))
-                {
-                    return Optional.of(currentCity);
-                }
-            }
-        }
-        return Optional.empty();
     }
 }
---
+++ src/main/java/info/tomfi/alexa/skills/shabbattimes/tools/CityLocator.java
@@ -0,0 +1,68 @@
+package info.tomfi.alexa.skills.shabbattimes.tools;
+
+import java.util.Iterator;
+import java.util.Optional;
+
+import com.amazon.ask.model.Slot;
+
+import info.tomfi.alexa.skills.shabbattimes.city.City;
+import info.tomfi.alexa.skills.shabbattimes.country.Country;
+import info.tomfi.alexa.skills.shabbattimes.country.CountryFactory;
+import info.tomfi.alexa.skills.shabbattimes.exception.NoCityFoundException;
+import info.tomfi.alexa.skills.shabbattimes.exception.NoCityInCountryException;
+import info.tomfi.alexa.skills.shabbattimes.tools.GlobalEnums.CountryInfo;
+
+public final class CityLocator
+{
+    private CityLocator()
+    {
+    }
+
+    public static City getByCityAndCountry(final Slot countrySlot, final Slot citySlot) throws NoCityFoundException, NoCityInCountryException
+    {
+        if (countrySlot.getValue() == null)
+        {
+            return getByCity(citySlot);
+        }
+        final Country country = CountryFactory.getCountry(countrySlot.getValue());
+        final Optional<City> cityOpt = findCityInCountry(country, citySlot.getValue());
+        if (cityOpt.isPresent())
+        {
+            return cityOpt.get();
+        }
+        throw new NoCityInCountryException(String.format("no city %s in %s.", citySlot.getValue(), countrySlot.getValue()));
+    }
+
+    private static City getByCity(final Slot citySlot)
+    {
+        for (CountryInfo member : CountryInfo.values())
+        {
+            final Country country = CountryFactory.getCountryByMember(member);
+            final Optional<City> cityOpt = findCityInCountry(country, citySlot.getValue());
+            if (cityOpt.isPresent())
+            {
+                return cityOpt.get();
+            }
+        }
+        throw new NoCityFoundException(String.format("city %s not found", citySlot.getValue()));
+    }
+
+    private static Optional<City> findCityInCountry(final Country country, final String cityName)
+    {
+        final Iterator<City> cities = country.iterator();
+        while (cities.hasNext())
+        {
+            final City currentCity = cities.next();
+            final Iterator<String> aliases = currentCity.iterator();
+            while (aliases.hasNext())
+            {
+                final String currentAlias = aliases.next();
+                if (currentAlias.equalsIgnoreCase(cityName))
+                {
+                    return Optional.of(currentCity);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+}
