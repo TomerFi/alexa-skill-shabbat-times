540d83807689f4bdeaffb65c8f076ffdadc15d76
Tomer Figenblat <tomer.figenblat@gmail.com>
2019-10-16 17:40:08 +0300

    Added session end handler test.

---
+++ src/test/java/info/tomfi/alexa/skills/shabbattimes/request/handlers/SessionEndHandlerTest.java
@@ -0,0 +1,52 @@
+package info.tomfi.alexa.skills.shabbattimes.request.handlers;
+
+import static info.tomfi.alexa.skills.shabbattimes.enums.Attributes.L10N_BUNDLE;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.RequestEnvelope;
+import com.amazon.ask.model.SessionEndedRequest;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public final class SessionEndHandlerTest
+{
+    private static SessionEndedRequest fakeRequest;
+    private static RequestEnvelope fakeEnvelope;
+    private static HandlerInput fakeInput;
+
+    private static SessionEndHandler handlerInTest;
+
+    @BeforeAll
+    public static void initialize()
+    {
+        fakeRequest = SessionEndedRequest.builder().build();
+        fakeEnvelope = RequestEnvelope.builder().withRequest(fakeRequest).build();
+        fakeInput = HandlerInput.builder().withRequestEnvelope(fakeEnvelope).build();
+
+        handlerInTest = new SessionEndHandler();
+    }
+
+    @Test
+    @DisplayName("test canHandle method implmentation")
+    public void canHandle_fakeArgs_returnTrue()
+    {
+        assertThat(handlerInTest.canHandle(fakeInput, fakeRequest)).isTrue();
+    }
+
+    @Test
+    @DisplayName("test handle method implementation")
+    public void handle_fakeArgs_validateResponse()
+    {
+        assertThat(handlerInTest.handle(fakeInput, fakeRequest).isPresent()).isFalse();
+    }
+}
