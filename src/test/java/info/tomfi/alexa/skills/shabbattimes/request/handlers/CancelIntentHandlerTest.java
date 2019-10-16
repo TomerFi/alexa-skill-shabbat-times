f8be8281b78cbe6838e92dc2683d6e1188b51c9a
Tomer Figenblat <tomer.figenblat@gmail.com>
2019-10-16 17:12:38 +0300

    Added cancel intent handler test.

---
+++ src/test/java/info/tomfi/alexa/skills/shabbattimes/request/handlers/CancelIntentHandlerTest.java
@@ -0,0 +1,62 @@
+package info.tomfi.alexa.skills.shabbattimes.request.handlers;
+
+import static info.tomfi.alexa.skills.shabbattimes.enums.Attributes.L10N_BUNDLE;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.RequestEnvelope;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import info.tomfi.alexa.skills.shabbattimes.enums.Intents;
+
+public final class CancelIntentHandlerTest
+{
+    private static Intent fakeIntent;
+    private static IntentRequest fakeRequest;
+    private static RequestEnvelope fakeEnvelope;
+    private static HandlerInput fakeInput;
+
+    private static CancelIntentHandler handlerInTest;
+
+    @BeforeAll
+    public static void initialize()
+    {
+        fakeIntent = Intent.builder().withName(Intents.CANCEL.getName()).build();
+        fakeRequest = IntentRequest.builder().withIntent(fakeIntent).build();
+        fakeEnvelope = RequestEnvelope.builder().withRequest(fakeRequest).build();
+        fakeInput = HandlerInput.builder().withRequestEnvelope(fakeEnvelope).build();
+
+        final ResourceBundle bundle = ResourceBundle.getBundle("locales/Responses", Locale.US);
+        final Map<String, Object> attributes = new HashMap<>();
+        attributes.put(L10N_BUNDLE.getName(), bundle);
+
+        fakeInput.getAttributesManager().setRequestAttributes(attributes);
+
+        handlerInTest = new CancelIntentHandler();
+    }
+
+    @Test
+    @DisplayName("test canHandle method implmentation")
+    public void canHandle_fakeArgs_returnTrue()
+    {
+        assertThat(handlerInTest.canHandle(fakeInput, fakeRequest)).isTrue();
+    }
+
+    @Test
+    @DisplayName("test handle method implementation")
+    public void handle_fakeArgs_validateResponse()
+    {
+        assertThat(handlerInTest.handle(fakeInput, fakeRequest).isPresent()).isTrue();
+    }
+}
