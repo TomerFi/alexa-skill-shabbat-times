4e56ec4a5b28012b80553e852529f46d67ce8e29
Tomer Figenblat <tomer.figenblat@gmail.com>
2019-10-20 00:04:04 +0300

    Added fallback intent test.

---
+++ src/test/java/info/tomfi/alexa/skills/shabbattimes/skilltests/FallbackIntentTest.java
@@ -0,0 +1,42 @@
+package info.tomfi.alexa.skills.shabbattimes.skilltests;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.amazon.ask.request.impl.BaseSkillRequest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import info.tomfi.alexa.skills.shabbattimes.ShabbatTimesSkillCreator;
+import info.tomfi.alexa.skills.shabbattimes.tools.DITestingConfiguration;
+import lombok.Cleanup;
+import lombok.val;
+
+public final class FallbackIntentTest
+{
+    @Test
+    @DisplayName("fallback intent test")
+    public void testFallbackIntent() throws BeansException, IllegalAccessException, InstantiationException, IOException, URISyntaxException
+    {
+        @Cleanup val context = new AnnotationConfigApplicationContext(DITestingConfiguration.class);
+        val skillInTest = context.getBean(ShabbatTimesSkillCreator.class).getSkill();
+
+        val input = Files.readAllBytes(Paths.get(FallbackIntentTest.class.getClassLoader()
+                .getResource("skill-tests/fallback_intent.json").toURI())
+        );
+        val response = skillInTest.execute(new BaseSkillRequest(input));
+
+        SkillResponseAssert.assertThat(response)
+            .isPresent()
+            .sessionIsOver()
+            .cardIsAbsent()
+            .sessionAttributesAreAbsent()
+            .repromptIsAbsent()
+            .outputSpeechIs("I'm sorry. I am unable to help you at the moment. Please try again later!");
+    }
+}
