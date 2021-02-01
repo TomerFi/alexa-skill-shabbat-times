18bb5c8e251d42352a7a97bd5b6274474d6fcfd8
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-02-01 17:19:14 +0200

    test: added handlers test cases

--- lambda/src/test/java/info/tomfi/alexa/shabbattimes/handlers/intent/FallbackIntentHandlerTest.java
+++ lambda/src/test/java/info/tomfi/alexa/shabbattimes/handlers/intent/FallbackIntentHandlerTest.java
@@ -12,14 +12,18 @@
  */
 package info.tomfi.alexa.shabbattimes.handlers.intent;
 
+import static info.tomfi.alexa.shabbattimes.BundleKey.DEFAULT_ERROR;
 import static info.tomfi.alexa.shabbattimes.IntentType.FALLBACK;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
 import static org.mockito.BDDMockito.given;
 
 import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.response.ResponseBuilder;
 import info.tomfi.alexa.shabbattimes.IntentType;
 import info.tomfi.alexa.shabbattimes.handlers.IntentHandlerFixtures;
+import java.util.Optional;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -49,5 +53,19 @@ final class FallbackIntentHandlerTest extends IntentHandlerFixtures {
     given(request.getIntent()).willReturn(intent);
     // verify handler cannot handle
     then(sut.canHandle(input, request)).isFalse();
+  }
+
+  @Test
+  void invoking_the_handler_should_play_a_prompt_and_end_the_session(
+      @Mock final ResponseBuilder builder, @Mock final Response response) {
+    // stub the builder with the steps expected to be performed by the sut
+    given(builder.withSpeech(getText(DEFAULT_ERROR))).willReturn(builder);
+    given(builder.withShouldEndSession(Boolean.TRUE)).willReturn(builder);
+    given(builder.build()).willReturn(Optional.of(response));
+    given(input.getResponseBuilder()).willReturn(builder);
+    // when invoking the handler
+    var resp = sut.handle(input, request);
+    // verify the mocked response return
+    then(resp).isNotEmpty().hasValue(response);
   }
 }
