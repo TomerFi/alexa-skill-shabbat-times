ca01c496cca5d9d03787775b1caf1690d051f57a
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-02-01 17:19:14 +0200

    test: added handlers test cases

--- lambda/src/test/java/info/tomfi/alexa/shabbattimes/handlers/intent/HelpIntentHandlerTest.java
+++ lambda/src/test/java/info/tomfi/alexa/shabbattimes/handlers/intent/HelpIntentHandlerTest.java
@@ -12,14 +12,19 @@
  */
 package info.tomfi.alexa.shabbattimes.handlers.intent;
 
+import static info.tomfi.alexa.shabbattimes.BundleKey.HELP_REPROMPT;
+import static info.tomfi.alexa.shabbattimes.BundleKey.HELP_SPEECH;
 import static info.tomfi.alexa.shabbattimes.IntentType.HELP;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
 import static org.mockito.BDDMockito.given;
 
 import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.response.ResponseBuilder;
 import info.tomfi.alexa.shabbattimes.IntentType;
 import info.tomfi.alexa.shabbattimes.handlers.IntentHandlerFixtures;
+import java.util.Optional;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -49,5 +54,20 @@ final class HelpIntentHandlerTest extends IntentHandlerFixtures {
     given(request.getIntent()).willReturn(intent);
     // verify handler cannot handle
     then(sut.canHandle(input, request)).isFalse();
+  }
+
+  @Test
+  void invoking_the_handler_should_return_a_follow_up(
+      @Mock final ResponseBuilder builder, @Mock final Response response) {
+    // stub the builder with the steps expected to be performed by the sut
+    given(builder.withSpeech(getText(HELP_SPEECH))).willReturn(builder);
+    given(builder.withReprompt(getText(HELP_REPROMPT))).willReturn(builder);
+    given(builder.withShouldEndSession(Boolean.FALSE)).willReturn(builder);
+    given(builder.build()).willReturn(Optional.of(response));
+    given(input.getResponseBuilder()).willReturn(builder);
+    // when invoking the handler
+    var resp = sut.handle(input, request);
+    // verify the mocked response return
+    then(resp).isNotEmpty().hasValue(response);
   }
 }
